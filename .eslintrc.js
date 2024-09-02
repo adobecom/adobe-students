fcfd0090652125a2d49100f64d3635cd39f5a845
sivasadobe <sivas@adobe.com>
2024-09-02 21:51:25 +0530

    feat: index,sitemap, path changed for sudents

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,7 @@
 module.exports = {
   root: true,
   extends: 'airbnb-base',
-  env: {
-    browser: true,
-  },
+  env: { browser: true, mocha: true },
   parser: '@babel/eslint-parser',
   parserOptions: {
     allowImportExportEverywhere: true,
@@ -11,8 +9,29 @@ module.exports = {
     requireConfigFile: false,
   },
   rules: {
-    'import/extensions': ['error', { js: 'always' }], // require js file extensions in imports
-    'linebreak-style': ['error', 'unix'], // enforce unix linebreaks
-    'no-param-reassign': [2, { props: false }], // allow modifying properties of param
+    // allow reassigning param
+    'no-param-reassign': [2, { props: false }],
+    'linebreak-style': ['error', 'unix'],
+    'import/extensions': ['error', { js: 'always' }],
+    'object-curly-newline': ['error', {
+      ObjectExpression: { multiline: true, minProperties: 6 },
+      ObjectPattern: { multiline: true, minProperties: 6 },
+      ImportDeclaration: { multiline: true, minProperties: 6 },
+      ExportDeclaration: 'never',
+    }],
+    'no-unused-expressions': 0,
+    'chai-friendly/no-unused-expressions': 2,
   },
+  overrides: [
+    {
+      files: ['test/**/*.js'],
+      rules: { 'no-console': 'off' },
+    },
+  ],
+  plugins: [
+    'chai-friendly',
+  ],
+  ignorePatterns: [
+    '/adobe-students/deps/*',
+  ],
 };
--- adobe-students/scripts/scripts.js
+++ adobe-students/scripts/scripts.js
@@ -1,132 +1,198 @@
+/*
+ * Copyright 2023 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
 import {
-  buildBlock,
-  loadHeader,
-  loadFooter,
-  decorateButtons,
-  decorateIcons,
-  decorateSections,
-  decorateBlocks,
-  decorateTemplateAndTheme,
-  waitForFirstImage,
-  loadSection,
-  loadSections,
-  loadCSS,
-  sampleRUM,
-} from './aem.js';
+  setLibs,
+  getUrlParams,
+} from './utils.js';
 
-/**
- * Builds hero block and prepends to main in a new section.
- * @param {Element} main The container element
+import { isTokenValid } from './goCart.js';
+
+import { decorateButton } from './decorate.js';
+const STYLES = '/adobe-students/styles/styles.css';
+
+// Use '/libs' if your live site maps '/libs' to milo's origin.
+const LIBS = '/libs';
+const noRedirect = new URLSearchParams(window.location.search).get('noRedirect');
+
+const locales = {
+  // Americas
+  ar: { ietf: 'es-AR', tk: 'oln4yqj.css' },
+  br: { ietf: 'pt-BR', tk: 'inq1xob.css' },
+  ca: { ietf: 'en-CA', tk: 'pps7abe.css' },
+  ca_fr: { ietf: 'fr-CA', tk: 'vrk5vyv.css' },
+  cl: { ietf: 'es-CL', tk: 'oln4yqj.css' },
+  co: { ietf: 'es-CO', tk: 'oln4yqj.css' },
+  la: { ietf: 'es-LA', tk: 'oln4yqj.css' },
+  mx: { ietf: 'es-MX', tk: 'oln4yqj.css' },
+  pe: { ietf: 'es-PE', tk: 'oln4yqj.css' },
+  '': { ietf: 'en-US', tk: 'hah7vzn.css' },
+  // EMEA
+  africa: { ietf: 'en', tk: 'pps7abe.css' },
+  be_fr: { ietf: 'fr-BE', tk: 'vrk5vyv.css' },
+  be_en: { ietf: 'en-BE', tk: 'pps7abe.css' },
+  be_nl: { ietf: 'nl-BE', tk: 'cya6bri.css' },
+  cy_en: { ietf: 'en-CY', tk: 'pps7abe.css' },
+  dk: { ietf: 'da-DK', tk: 'aaz7dvd.css' },
+  de: { ietf: 'de-DE', tk: 'vin7zsi.css' },
+  ee: { ietf: 'et-EE', tk: 'aaz7dvd.css' },
+  es: { ietf: 'es-ES', tk: 'oln4yqj.css' },
+  fr: { ietf: 'fr-FR', tk: 'vrk5vyv.css' },
+  gr_en: { ietf: 'en-GR', tk: 'pps7abe.css' },
+  ie: { ietf: 'en-GB', tk: 'pps7abe.css' },
+  il_en: { ietf: 'en-IL', tk: 'pps7abe.css' },
+  it: { ietf: 'it-IT', tk: 'bbf5pok.css' },
+  lv: { ietf: 'lv-LV', tk: 'aaz7dvd.css' },
+  lt: { ietf: 'lt-LT', tk: 'aaz7dvd.css' },
+  lu_de: { ietf: 'de-LU', tk: 'vin7zsi.css' },
+  lu_en: { ietf: 'en-LU', tk: 'pps7abe.css' },
+  lu_fr: { ietf: 'fr-LU', tk: 'vrk5vyv.css' },
+  hu: { ietf: 'hu-HU', tk: 'aaz7dvd.css' },
+  mt: { ietf: 'en-MT', tk: 'pps7abe.css' },
+  mena_en: { ietf: 'en', tk: 'pps7abe.css' },
+  nl: { ietf: 'nl-NL', tk: 'cya6bri.css' },
+  no: { ietf: 'no-NO', tk: 'aaz7dvd.css' },
+  pl: { ietf: 'pl-PL', tk: 'aaz7dvd.css' },
+  pt: { ietf: 'pt-PT', tk: 'inq1xob.css' },
+  ro: { ietf: 'ro-RO', tk: 'aaz7dvd.css' },
+  sa_en: { ietf: 'en', tk: 'pps7abe.css' },
+  ch_de: { ietf: 'de-CH', tk: 'vin7zsi.css' },
+  si: { ietf: 'sl-SI', tk: 'aaz7dvd.css' },
+  sk: { ietf: 'sk-SK', tk: 'aaz7dvd.css' },
+  ch_fr: { ietf: 'fr-CH', tk: 'vrk5vyv.css' },
+  fi: { ietf: 'fi-FI', tk: 'aaz7dvd.css' },
+  se: { ietf: 'sv-SE', tk: 'fpk1pcd.css' },
+  ch_it: { ietf: 'it-CH', tk: 'bbf5pok.css' },
+  tr: { ietf: 'tr-TR', tk: 'aaz7dvd.css' },
+  ae_en: { ietf: 'en', tk: 'pps7abe.css' },
+  uk: { ietf: 'en-GB', tk: 'pps7abe.css' },
+  at: { ietf: 'de-AT', tk: 'vin7zsi.css' },
+  cz: { ietf: 'cs-CZ', tk: 'aaz7dvd.css' },
+  bg: { ietf: 'bg-BG', tk: 'aaz7dvd.css' },
+  ru: { ietf: 'ru-RU', tk: 'aaz7dvd.css' },
+  ua: { ietf: 'uk-UA', tk: 'aaz7dvd.css' },
+  il_he: { ietf: 'he', tk: 'nwq1mna.css', dir: 'rtl' },
+  ae_ar: { ietf: 'ar', tk: 'nwq1mna.css', dir: 'rtl' },
+  mena_ar: { ietf: 'ar', tk: 'dis2dpj.css', dir: 'rtl' },
+  sa_ar: { ietf: 'ar', tk: 'nwq1mna.css', dir: 'rtl' },
+  // Asia Pacific
+  au: { ietf: 'en-AU', tk: 'pps7abe.css' },
+  hk_en: { ietf: 'en-HK', tk: 'pps7abe.css' },
+  in: { ietf: 'en-GB', tk: 'pps7abe.css' },
+  id_id: { ietf: 'id', tk: 'czc0mun.css' },
+  id_en: { ietf: 'en', tk: 'pps7abe.css' },
+  my_ms: { ietf: 'ms', tk: 'sxj4tvo.css' },
+  my_en: { ietf: 'en-GB', tk: 'pps7abe.css' },
+  nz: { ietf: 'en-GB', tk: 'pps7abe.css' },
+  ph_en: { ietf: 'en', tk: 'pps7abe.css' },
+  ph_fil: { ietf: 'fil-PH', tk: 'ict8rmp.css' },
+  sg: { ietf: 'en-SG', tk: 'pps7abe.css' },
+  th_en: { ietf: 'en', tk: 'pps7abe.css' },
+  in_hi: { ietf: 'hi', tk: 'aaa8deh.css' },
+  th_th: { ietf: 'th', tk: 'aaz7dvd.css' },
+  cn: { ietf: 'zh-CN', tk: 'puu3xkp' },
+  hk_zh: { ietf: 'zh-HK', tk: 'jay0ecd' },
+  tw: { ietf: 'zh-TW', tk: 'jay0ecd' },
+  jp: { ietf: 'ja-JP', tk: 'dvg6awq' },
+  kr: { ietf: 'ko-KR', tk: 'qjs5sfm' },
+  // Langstore Support.
+  langstore: { ietf: 'en-US', tk: 'hah7vzn.css' },
+  // geo expansion MWPW-125686
+  za: { ietf: 'en-GB', tk: 'pps7abe.css' }, // South Africa (GB English)
+  ng: { ietf: 'en-GB', tk: 'pps7abe.css' }, // Nigeria (GB English)
+  cr: { ietf: 'es-419', tk: 'oln4yqj.css' }, // Costa Rica (Spanish Latin America)
+  ec: { ietf: 'es-419', tk: 'oln4yqj.css' }, // Ecuador (Spanish Latin America)
+  pr: { ietf: 'es-419', tk: 'oln4yqj.css' }, // Puerto Rico (Spanish Latin America)
+  gt: { ietf: 'es-419', tk: 'oln4yqj.css' }, // Guatemala (Spanish Latin America)
+  eg_ar: { ietf: 'ar', tk: 'nwq1mna.css', dir: 'rtl' }, // Egypt (Arabic)
+  kw_ar: { ietf: 'ar', tk: 'nwq1mna.css', dir: 'rtl' }, // Kuwait (Arabic)
+  qa_ar: { ietf: 'ar', tk: 'nwq1mna.css', dir: 'rtl' }, // Qatar (Arabic)
+  eg_en: { ietf: 'en-GB', tk: 'pps7abe.css' }, // Egypt (GB English)
+  kw_en: { ietf: 'en-GB', tk: 'pps7abe.css' }, // Kuwait (GB English)
+  qa_en: { ietf: 'en-GB', tk: 'pps7abe.css' }, // Qatar (GB English)
+  gr_el: { ietf: 'el', tk: 'fnx0rsr.css' }, // Greece (Greek)
+  vn_en: { ietf: 'en-GB', tk: 'hah7vzn.css' },
+  vn_vi: { ietf: 'vi', tk: 'qxw8hzm.css' },
+  cis_ru: { ietf: 'ru', tk: 'qxw8hzm.css' },
+  cis_en: { ietf: 'en', tk: 'pps7abe.css' },
+};
+
+// Add any config options.
+const CONFIG = {
+  contentRoot: '/students-shared',
+  codeRoot: '/adobe-students',
+  imsClientId: 'adobedotcom-cc',
+  locales,
+  geoRouting: 'on',
+  prodDomains: ['www.adobe.com', 'helpx.adobe.com', 'business.adobe.com', 'www.adobe-students.com'],
+  placeholders: getUrlParams(),
+  stage: {
+    marTechUrl:
+      'https://assets.adobedtm.com/d4d114c60e50/a0e989131fd5/launch-2c94beadc94f-development.min.js',
+    edgeConfigId: 'e065836d-be57-47ef-b8d1-999e1657e8fd',
+    pdfViewerClientId: '9f7f19a46bd542e2b8548411e51eb4d4',
+    pdfViewerReportSuite: 'adbadobenonacdcqa',
+  },
+  prod: {
+    marTechUrl: 'https://assets.adobedtm.com/d4d114c60e50/a0e989131fd5/launch-5dd5dd2177e6.min.js',
+    edgeConfigId: '2cba807b-7430-41ae-9aac-db2b0da742d5',
+    pdfViewerClientId: '409019ebd2d546c0be1a0b5a61fe65df',
+    pdfViewerReportSuite: 'adbadobenonacdcprod',
+  },
+  jarvis: {
+    id: 'adobedotcom2',
+    version: '1.83',
+    onDemand: false,
+  },
+};
+
+/*
+ * ------------------------------------------------------------
+ * Edit below at your own risk
+ * ------------------------------------------------------------
  */
-function buildHeroBlock(main) {
-  const h1 = main.querySelector('h1');
-  const picture = main.querySelector('picture');
-  // eslint-disable-next-line no-bitwise
-  if (h1 && picture && (h1.compareDocumentPosition(picture) & Node.DOCUMENT_POSITION_PRECEDING)) {
-    const section = document.createElement('div');
-    section.append(buildBlock('hero', { elems: [picture, h1] }));
-    main.prepend(section);
+
+const miloLibs = setLibs(LIBS);
+const { loadArea, setConfig, loadLana } = await import(
+  `${miloLibs}/utils/utils.js`
+);
+
+setConfig({ ...CONFIG, miloLibs });
+
+async function loadAdobeStudentsPage() {
+  loadLana({ clientId: 'adobe-students' });
+  await loadArea();
+  decorateButton();
+}
+
+(function loadStyles() {
+  const paths = [`${miloLibs}/styles/styles.css`];
+  if (STYLES) {
+    paths.push(STYLES);
   }
-}
+  paths.forEach((path) => {
+    const link = document.createElement('link');
+    link.setAttribute('rel', 'stylesheet');
+    link.setAttribute('href', path);
+    document.head.appendChild(link);
+  });
+})();
 
-/**
- * load fonts.css and set a session storage flag
- */
-async function loadFonts() {
-  await loadCSS(`${window.hlx.codeBasePath}/styles/fonts.css`);
-  try {
-    if (!window.location.hostname.includes('localhost')) sessionStorage.setItem('fonts-loaded', 'true');
-  } catch (e) {
-    // do nothing
+(async function loadPage() {
+  const validate = document.head.querySelector(`meta[name="validate"]`);
+  if (validate?.content === 'on') {
+    if (await isTokenValid(miloLibs) || noRedirect) return loadAdobeStudentsPage();
+    const defaultPage = document.head.querySelector(`meta[name="default-page"]`);
+    window.location.href = defaultPage?.content || 'https://www.adobe-students.com/';
   }
-}
-
-/**
- * Builds all synthetic blocks in a container element.
- * @param {Element} main The container element
- */
-function buildAutoBlocks(main) {
-  try {
-    buildHeroBlock(main);
-  } catch (error) {
-    // eslint-disable-next-line no-console
-    console.error('Auto Blocking failed', error);
-  }
-}
-
-/**
- * Decorates the main element.
- * @param {Element} main The main element
- */
-// eslint-disable-next-line import/prefer-default-export
-export function decorateMain(main) {
-  // hopefully forward compatible button decoration
-  decorateButtons(main);
-  decorateIcons(main);
-  buildAutoBlocks(main);
-  decorateSections(main);
-  decorateBlocks(main);
-}
-
-/**
- * Loads everything needed to get to LCP.
- * @param {Element} doc The container element
- */
-async function loadEager(doc) {
-  document.documentElement.lang = 'en';
-  decorateTemplateAndTheme();
-  const main = doc.querySelector('main');
-  if (main) {
-    decorateMain(main);
-    document.body.classList.add('appear');
-    await loadSection(main.querySelector('.section'), waitForFirstImage);
-  }
-
-  sampleRUM.enhance();
-
-  try {
-    /* if desktop (proxy for fast connection) or fonts already loaded, load fonts.css */
-    if (window.innerWidth >= 900 || sessionStorage.getItem('fonts-loaded')) {
-      loadFonts();
-    }
-  } catch (e) {
-    // do nothing
-  }
-}
-
-/**
- * Loads everything that doesn't need to be delayed.
- * @param {Element} doc The container element
- */
-async function loadLazy(doc) {
-  const main = doc.querySelector('main');
-  await loadSections(main);
-
-  const { hash } = window.location;
-  const element = hash ? doc.getElementById(hash.substring(1)) : false;
-  if (hash && element) element.scrollIntoView();
-
-  loadHeader(doc.querySelector('header'));
-  loadFooter(doc.querySelector('footer'));
-
-  loadCSS(`${window.hlx.codeBasePath}/styles/lazy-styles.css`);
-  loadFonts();
-}
-
-/**
- * Loads everything that happens a lot later,
- * without impacting the user experience.
- */
-function loadDelayed() {
-  // eslint-disable-next-line import/no-cycle
-  window.setTimeout(() => import('./delayed.js'), 3000);
-  // load anything that can be postponed to the latest here
-}
-
-async function loadPage() {
-  await loadEager(document);
-  await loadLazy(document);
-  loadDelayed();
-}
-
-loadPage();
+  loadAdobeStudentsPage();
+})();
